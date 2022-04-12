e12312077efe411aafae3345f855753d45931b47
John <nadenjohn@gmail.com>
2022-04-12 14:30:19 -0700

    commit tree 060424cf76b3020682a56d3cf784321fd01612fd

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,23 @@
-class Dog
+# require "active_record"
+# require_relative "../db/test.sqlite3"
+
+class Dog < ActiveRecord::Base
+
+    
+    # ActiveRecord::Base.establish_connection(
+    #     adapter: "sqlite3",
+    #     database: "../db/test.sqlite3"
+    # )
+    # sql = <<-SQL
+    # CREATE TABLE IF NOT EXISTS dogs(
+    #     id INTEGER PRIMARY KEY, 
+    #     name TEXT, 
+    #     breed TEXT
+    # )
+    # SQL
+    # ActiveRecord::Base.connection.execute(sql)
+    # ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+   
 end
+
